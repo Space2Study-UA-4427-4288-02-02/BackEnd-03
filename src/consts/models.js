54197632e58cf968f86d285fc27fbdc59380b1ce
Yaroslav Tkachuk <iaroslav.tkachuk.90@gmail.com>
2025-10-15 23:38:52 +0300

    #8-removeOldPhotoFromCloudinary

--- src/consts/models.js
+++ src/consts/models.js
@@ -12,7 +12,8 @@ const refs = {
   FINISHED_QUIZ: 'FinishedQuiz',
   ATTACHMENT: 'Attachment',
   QUESTION: 'Question',
-  RESOURCES_CATEGORY: 'ResourcesCategory'
+  RESOURCES_CATEGORY: 'ResourcesCategory',
+  IMAGE: 'Image'
 }
 
 module.exports = refs
